f86d2f5ca460bc0981698f2db17b1eb587635386
slacker1992 <suvinjavax@gmail.com>
2023-02-15 11:33:29 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 "use client";
-import { Inter } from "@next/font/google";
 import Box from "@mui/material/Box";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return <Box>Home Page </Box>;
